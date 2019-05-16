436c3048455fd148049d82402cd8921da972e9f7
tomatillodesign <chris@tomatillodesign.com>
2019-05-16 10:53:48 -0400

    getting close, needs style

--- src/clb-icon-card/block.js
+++ src/clb-icon-card/block.js
@@ -66,20 +66,59 @@ export default registerBlockType(
               type: 'string',
               default: '#555d66'
           },
+          icon: {
+               type: 'string',
+               default: 'info-circle'
+          },
+          iconSize: {
+               type: 'string',
+               default: '4x'
+          },
+          iconStyle: {
+               type: 'string',
+               default: 'far'
+          },
         },
         edit: props => {
-            const { attributes: { iconCardTitle, iconCardLink, backgroundColor },
+            const { attributes: { iconCardTitle, iconCardLink, icon, iconSize, iconStyle, backgroundColor },
                 className, setAttributes, isSelected } = props;
 
             const onChangeIconCardTitle = iconCardTitle => { setAttributes( { iconCardTitle } ) };
             const onChangeIconCardLink = iconCardLink => { setAttributes( { iconCardLink } ) };
+            const onChangeIcon = icon => { setAttributes( { icon } ) };
+            const onChangeIconSize = iconSize => { setAttributes( { iconSize } ) };
+            const onChangeIconStyle = iconStyle => { setAttributes( { iconStyle } ) };
 
             return (
 			  <Fragment>
                  <InspectorControls>
                    <PanelBody
-                       title={ __( 'Button Settings', 'clbbutton' ) }
+                       title={ __( 'Icon Card Settings', 'clb-icon-card' ) }
                    >
+                   <TextControl
+                       label={ 'Icon' }
+                       help={ 'Copy the icon text from fontawesome.com/icons. Eg: lightbulb-on' }
+                       value={ icon }
+                       onChange={ onChangeIcon }
+                   />
+                   <TextControl
+                       label={ 'Icon Size' }
+                       help={ 'Select the size of your icon. See: https://fontawesome.com/how-to-use/on-the-web/styling/sizing-icons' }
+                       value={ iconSize }
+                       onChange={ onChangeIconSize }
+                   />
+                   <SelectControl
+                       label={ 'Icon Style' }
+                       help={ 'Select the size of your icon. See: https://fontawesome.com/how-to-use/on-the-web/styling/sizing-icons' }
+                       value={ iconStyle }
+                       onChange={ onChangeIconStyle }
+                       options={[
+                        { label: "Solid", value: "fas" },
+                        { label: "Regular", value: "far" },
+                        { label: "Light", value: "fal" },
+                        { label: "Brand", value: "fab" }
+                      ]}
+                   />
                        <PanelRow>
                             <PanelColorSettings
                             title={'Background Color'}
@@ -128,13 +167,14 @@ export default registerBlockType(
 
         save: props => {
 
-            const { iconCardTitle, iconCardLink, backgroundColor } = props.attributes;
+            const { iconCardTitle, iconCardLink, icon, iconSize, iconStyle, backgroundColor } = props.attributes;
 
             return (
 
                  <div className="clb-icon-card-area" >
                       <a href={iconCardLink} className="button full clb-icon-card" style={ { backgroundColor: backgroundColor } }>
-                               <div className="clb_card__title">{iconCardTitle}</div>
+                               <div className="clb-card-icon"><i className={ `${iconStyle} fa-${icon} fa-${iconSize}` }></i></div>
+                               <h3 className="clb_card__title">{iconCardTitle}</h3>
                          </a>
                     </div>
 
